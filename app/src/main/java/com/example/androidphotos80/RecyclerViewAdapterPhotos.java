98b12cc6ef7d77ee81cb56d416450030c4a1d656
nick-whelan <nwhelan97@gmail.com>
2020-12-06 12:34:41 -0500

    photo adapter

--- app/src/main/java/com/example/androidphotos80/RecyclerViewAdapterPhotos.java
+++ app/src/main/java/com/example/androidphotos80/RecyclerViewAdapterPhotos.java
@@ -1,30 +1,39 @@
 package com.example.androidphotos80;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.androidphotos80.model.Album;
+import com.example.androidphotos80.model.Photo;
 
 import java.util.List;
-/*
+
 public class RecyclerViewAdapterPhotos extends RecyclerView.Adapter<RecyclerViewAdapterPhotos.ViewHolder> {
 
     private List<Album> albumList;
     private RecyclerViewAdapter.OnNoteListener mOnNoteListener;
+    private Context context;
+    private Album album;
+    private List<Photo> photoList;
 
-   /*
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        // Need to make the layout list item and attach below to view
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_photolistitem, parent , false);
+        RecyclerViewAdapterPhotos.ViewHolder holder  = new RecyclerViewAdapterPhotos.ViewHolder(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        // Load image into imageview
     }
 
     @Override
@@ -33,8 +42,16 @@ public class RecyclerViewAdapterPhotos extends RecyclerView.Adapter<RecyclerView
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-      //  OnNoteListener onNoteListener;
-      //  Button
+
+        ImageView thumbnail;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            thumbnail = itemView.findViewById(R.id.photoThumbnail);
+
+            // All of our buttons connecting to the stuff
+
+        }
 
         @Override
         public void onClick(View view) {
@@ -42,4 +59,3 @@ public class RecyclerViewAdapterPhotos extends RecyclerView.Adapter<RecyclerView
         }
     }
 }
-*/
