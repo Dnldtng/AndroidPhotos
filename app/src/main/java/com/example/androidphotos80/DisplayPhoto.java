f6a54265bb19d94b2a6ecc2a52ff07416f018759
Dnldtng <donaldtong077@gmail.com>
2020-12-08 17:14:13 -0500

    linked display photos, and delete button

--- app/src/main/java/com/example/androidphotos80/DisplayPhoto.java
+++ app/src/main/java/com/example/androidphotos80/DisplayPhoto.java
@@ -1,22 +1,37 @@
 package com.example.androidphotos80;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.androidphotos80.model.Photo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DisplayPhoto extends AppCompatActivity {
 
     private TextView mTextView;
+    private List<Photo> photoList = new ArrayList<>();
+    private int selectedPhotoIndex;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display_photos);
-
         mTextView = (TextView) findViewById(R.id.text);
+        Intent intent = getIntent();
+        photoList = (ArrayList<Photo>) intent.getSerializableExtra("photoList");
+        selectedPhotoIndex = intent.getIntExtra("selectedPhotoIndex", 0);
 
-
+        Photo selectedPhoto = photoList.get(selectedPhotoIndex);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        imageView.setImageURI(Uri.parse(selectedPhoto.getPath()));
     }
 }
--- app/src/main/java/com/example/androidphotos80/OpenedAlbum.java
+++ app/src/main/java/com/example/androidphotos80/OpenedAlbum.java
@@ -1,6 +1,8 @@
 package com.example.androidphotos80;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -19,8 +21,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import java.io.File;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.ToDoubleBiFunction;
@@ -33,12 +38,31 @@ public class OpenedAlbum extends AppCompatActivity {
     private int albumIndex;
     private List<Photo> photoList = new ArrayList<>();
     private Photo selectedPhoto;
+    private int selectedPhotoIndex;
 
     public void addButton(View view){
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("image/*");
         startActivityForResult(intent, 20);
+    }
+
+    public void deleteButton(View view){
+        photoList.remove(selectedPhotoIndex);
+        adapter.notifyItemRemoved(selectedPhotoIndex);
+        System.out.println("removed");
+        //adapter.notifyDataSetChanged();
+    }
+
+    public void displayButton(View view){
+        Intent intent = new Intent (this, DisplayPhoto.class);
+        intent.putExtra("photoList", (Serializable) photoList);
+        intent.putExtra("selectedPhotoIndex", selectedPhotoIndex);
+        startActivity(intent);
+    }
+
+    public void moveButton(View view){
+
     }
 
     @Override
@@ -73,32 +97,27 @@ public class OpenedAlbum extends AppCompatActivity {
         albumIndex = intent.getIntExtra("albumPosition", 0);
         recyclerView = findViewById(R.id.recyclerView2);
 
-
         //this was causing null pointer because no adapter attached
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
         selectedAlbum = albumList.get(albumIndex);
-
-
-
-        //prob dont need
-        //!!!omg this dumb ass line was fucking it up!
-        //photoList = selectedAlbum.getPhotosList();
 
         adapter = new RecyclerViewAdapterPhotos(this, photoList);
         recyclerView.setAdapter(adapter);
-
         //had to replace with lambdas for some reason
         //adapter.setOnItemClickListener(new RecyclerViewAdapterPhotos.OnItemClickListener() {
 
         adapter.setOnItemClickListener(position -> {
+            selectedPhotoIndex = position;
             selectedPhoto = photoList.get(position);
             System.out.println("selected");
 
             //TODO highlight the selected item
         });
 
-        //TODO add a recyclerView for the photos, be able to first load/save the photos, then make it so we can click the cells, also need to make the buttons open up dialogs.
+
+
+
+    //TODO add a recyclerView for the photos, be able to first load/save the photos, then make it so we can click the cells, also need to make the buttons open up dialogs.
 
     }
 
